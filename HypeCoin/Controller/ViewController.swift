991a36770785d06d1192140e4c73d67b72547d6a
Darren Rambaud <xyzst@users.noreply.github.com>
2020-02-17 19:11:17 -0600

    Initialize iOS/Swift Project
    Commit common xcode files. Add basic storyboard.
    
    Related to: none

---
+++ HypeCoin/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HypeCoin
+//
+//  Created by Darren Rambaud on 02/17/2020.
+//  Copyright © 2020 Darren Rambaud. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ HypeCoin/Model/CoinManager.swift
@@ -0,0 +1,19 @@
+//
+//  CoinManager.swift
+//  HypeCoin
+//
+//  Created by Darren Rambaud on 02/17/2020.
+//  Copyright © 2020 Darren Rambaud. All rights reserved.
+//
+
+import Foundation
+
+struct CoinManager {
+    
+    let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
+    let apiKey = "YOUR_API_KEY_HERE"
+    
+    let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+
+    
+}
