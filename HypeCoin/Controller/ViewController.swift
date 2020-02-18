03654e64d197479247609a487e0eca67e6aa7bf3
Darren Rambaud <xyzst@users.noreply.github.com>
2020-02-17 19:42:34 -0600

    Define IBActions/Outlets
    Add delegate for UIPicker, use extensions to abstract out delegate related methods out of view controller. Define several UIKit elements and assign them to IBActions/Outlets.
    
    Update model class w/ helper methods. The app is not functional at this point, however the UI picker is now pre-populated with a list of convertible currencies.
    
    Related to: none

--- HypeCoin/Controller/ViewController.swift
+++ HypeCoin/Controller/ViewController.swift
@@ -10,11 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var bitcoin: UILabel!
+    @IBOutlet weak var currency: UILabel!
+    @IBOutlet weak var toCurrency: UIPickerView!
+    
+    let coinManager = CoinManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        toCurrency.dataSource = self
+        toCurrency.delegate = self
     }
+}
 
-
+// MARK: - UIPickerViewDataSource
+extension ViewController: UIPickerViewDataSource, UIPickerViewDelegate {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1 // # of columns
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return coinManager.currencies.count // # of rows
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return coinManager.currencies[row] // for currently selected item in picker, give me (n-1)th item
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        coinManager.getPrice(for: coinManager.getCurrency(nth: row))
+    }
 }
 
--- HypeCoin/Model/CoinManager.swift
+++ HypeCoin/Model/CoinManager.swift
@@ -11,9 +11,15 @@ import Foundation
 struct CoinManager {
     
     let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
-    let apiKey = "YOUR_API_KEY_HERE"
+    let apiKey = "YOUR_API_KEY_HERE" // !!! DO NOT COMMIT THE API KEY TO THE REPOSITORY !!!
     
-    let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+    let currencies = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
 
+    func getPrice(for currency: String) {
+        print(currency)
+    }
     
+    func getCurrency(nth: Int) -> String {
+        return nth >= 0 && nth < currencies.count ? currencies[nth] : "USD"
+    }
 }
